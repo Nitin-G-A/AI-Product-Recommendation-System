1e03ff772208b5c35f1b25917b9b0d9c4b4bef1e
Nitin-G-A <nitinga2005@gmail.com>
2026-06-08 22:31:13 +0530

    Phase 4.5 for age group update

--- ai-service/app.py
+++ ai-service/app.py
@@ -29,8 +29,20 @@ def analyze():
     age = result[0]["age"]
     gender = result[0]["dominant_gender"]
 
+    if age < 13:
+     age_group = "Child"
+    elif age < 20:
+     age_group = "Teen"
+    elif age < 36:
+     age_group = "Young Adult"
+    elif age < 60:
+     age_group = "Adult"
+    else:
+     age_group = "Senior"
+
     print("Age:", age)
     print("Gender:", gender)
+    print("Age Group:", age_group)
     print("Saving to database...")
 
     conn = get_connection()
@@ -38,11 +50,11 @@ def analyze():
 
     cursor.execute(
     """
-    INSERT INTO customers(age, gender, income)
-    VALUES (%s, %s, %s)
+    INSERT INTO customers(age, gender, income, age_group)
+    VALUES (%s, %s, %s, %s)
     """,
-    (age, gender, income)
-    )
+    (age, gender, income, age_group)
+   )
 
     conn.commit()
     print("Saved successfully!")
@@ -53,10 +65,11 @@ def analyze():
     os.remove(image_path)
 
     return jsonify({
-        "age": age,
-        "gender": gender,
-        "income": income
-    })
+    "age": age,
+    "age_group": age_group,
+    "gender": gender,
+    "income": income
+   })
 
 if __name__ == "__main__":
     app.run(port=5001, debug=True)
